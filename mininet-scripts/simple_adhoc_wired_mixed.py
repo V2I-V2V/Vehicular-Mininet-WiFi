5ba374b1d669db047d76d7bfc7f3d678c176fd92
ry4nzhu <ryanzhu@umich.edu>
2021-06-15 23:41:03 -0400

    adhoc node mixed with host example works

--- mininet-scripts/simple_adhoc_wired_mixed.py
+++ mininet-scripts/simple_adhoc_wired_mixed.py
@@ -10,55 +10,37 @@ from mn_wifi.wmediumdConnector import interference
 
 def topology(args):
     net = Mininet_wifi(link=wmediumd, wmediumd_mode=interference)
-
-    info( '*** Add switches\n')
-    s1 = net.addSwitch('s1')
-
     info("*** Creating nodes\n")
-    server = net.addHost('server', mac='00:00:00:00:00:01', ip6='fe80::3', 
+    server = net.addHost('server', mac='00:00:00:00:00:01', ip='192.168.0.1/24',
                         position='15,15,0')
     sta1 = net.addStation('sta1', mac='00:00:00:00:00:02', ip6='fe80::1',
                         position='10,10,0')
     sta2 = net.addStation('sta2', mac='00:00:00:00:00:03', ip6='fe80::2',
-                        position='50,10,0')                        
-
-
+                        position='50,10,0') 
     net.setPropagationModel(model="logDistance", exp=4)
-
-    # plot the topology
-    info("*** Plotting Graph\n")
-    net.plotGraph(max_x=200, max_y=200)
 
     info("*** Configuring wifi nodes\n")
     net.configureWifiNodes()
 
     info("*** Creating links\n")
-    # MANET routing protocols supported by proto:
-    # babel, batman_adv, batmand and olsr
-    # WARNING: we may need to stop Network Manager if you want
-    # to work with babel
-    protocols = ['babel', 'batman_adv', 'batmand', 'olsrd', 'olsrd2']
     kwargs = dict()
-    for proto in args:
-        if proto in protocols:
-            kwargs['proto'] = proto
-    
-    net.addLink(sta1, s1, cls=TCLink, intf='sta1-eth1')
-    net.addLink(server, s1, cls=TCLink)
+    kwargs['proto'] = 'olsrd'
     net.addLink(sta1, cls=adhoc, intf='sta1-wlan0',
-                ssid='adhocNet', mode='g', channel=5,
-                **kwargs)
+                ssid='adhocNet', mode='g', channel=5, **kwargs)
     net.addLink(sta2, cls=adhoc, intf='sta2-wlan0',
-                ssid='adhocNet', mode='g', channel=5,
-                **kwargs)
-    
-    info("\n*** Addressing...\n")
-    if 'proto' not in kwargs:
-        sta1.setIP6('2001::1/64', intf="sta1-wlan0")
-        sta2.setIP6('2001::2/64', intf="sta2-wlan0")
-        sta1.setIP('10.0.0.4/8', intf="sta1-eth1")
-        # server.setIP6('2001::3/64', intf="server-eth0")
-    
+                ssid='adhocNet', mode='g', channel=5, **kwargs)
+    net.addLink(sta1, server, cls=TCLink)
+
+
+    info("*** Addressing...\n")
+    sta1.setIP6('2001::1/64', intf="sta1-wlan0")
+    sta2.setIP6('2001::2/64', intf="sta2-wlan0")
+    sta1.setIP('192.168.0.4/24', intf="sta1-eth1")
+
+    sta1.cmd('echo 1 > /proc/sys/net/ipv4/ip_forward')
+    server.cmd('echo 1 > /proc/sys/net/ipv4/ip_forward')
+
+
     info("*** Starting network\n")
     net.build()
 
@@ -68,7 +50,6 @@ def topology(args):
     info("*** Stopping network\n")
     net.stop()
 
-
 if __name__ == '__main__':
     setLogLevel('info')
-    topology(sys.argv)
+    topology(sys.argv)    
