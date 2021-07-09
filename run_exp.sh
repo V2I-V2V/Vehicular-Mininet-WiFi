80b7207961f74e9aa8c675b125d4600caf312538
ry4nzhu <ryanzhu@umich.edu>
2021-07-09 01:22:03 -0400

    update run_exp.sh

--- run_exp.sh
+++ run_exp.sh
@@ -2,7 +2,7 @@ for t in 1 2 3 4
 do
     echo $t
     # run an experiment
-    sudo python3 vehicular_perception.py -n 6 -p input/pcds/pcd-data-config.txt -l input/locations/location-example-full.txt --trace input/traces/trace-mindist-bw.txt --helpee_conf ./input/helpee_conf/helpee-nodes.txt --run_app -s minDist -t 90 --fps 10
+    sudo python3 vehicular_perception.py -n 6 -p input/pcds/pcd-data-config.txt -l input/locations/location-mindist-bw.txt --trace input/traces/trace-mindist-bw.txt --helpee_conf ./input/helpee_conf/helpee-nodes.txt --run_app -s minDist -t 90 --fps 5
     # copy output data to another directory
     tag=$(date +"%m%d%H%M%S")
     mkdir ~/v2x
@@ -10,5 +10,5 @@ do
     cp -r logs ~/v2x/data-$tag/
     cp -r output ~/v2x/data-$tag/
     cp -r pcaps ~/v2x/data-$tag/
-    python3 analysis-scripts/calc_delay.py ~/v2x/data-$tag/ 6 240
+    python3 analysis-scripts/calc_delay.py ~/v2x/data-$tag/ 6 120
 done
