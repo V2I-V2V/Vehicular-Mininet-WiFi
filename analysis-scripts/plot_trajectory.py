bca624d16acff451001f5b86403a116b47cf3b5e
ry4nzhu <ryanzhu@umich.edu>
2021-07-15 13:09:34 -0400

    group results

--- analysis-scripts/plot_trajectory.py
+++ analysis-scripts/plot_trajectory.py
@@ -19,17 +19,18 @@ ax = fig.add_subplot(111)
 for i in range(int(traj.shape[1]/2)):
     # ax.plot(traj[:,2*i], traj[:,2*i+1], label='node%d'%i)
     ax.scatter([traj[:,2*i][0]], [traj[:,2*i+1][0]], s=184, label='node%d'%i)
+    ax.scatter(traj[:,2*i], traj[:,2*i+1], s=84, label='node%d'%i)
     print(traj[:,2*i+1][-1])
     # ax.scatter(traj[:,2*i][-1], traj[:,2*i+1][-1])
     trajs[i] = traj[:,2*i:2*i+2]
-plt.xlim([-10, 50])
-plt.ylim([-10, 100])
+# plt.xlim([-10, 50])
+# plt.ylim([-10, 100])
 plt.xlabel('x (m)')
 plt.ylabel('y (m)')
 plt.legend()
 plt.tight_layout()
 
-plt.savefig('traj-start.png')
+plt.savefig('traj.png')
 
 fig = plt.figure(figsize=(7,7))
 ax = fig.add_subplot(111)
@@ -41,8 +42,8 @@ for i in range(int(traj.shape[1]/2)):
     # ax.scatter(traj[:,2*i][-1], traj[:,2*i+1][-1])
     trajs[i] = traj[:,2*i:2*i+2]
 
-plt.xlim([-10, 50])
-plt.ylim([360, 500])
+# plt.xlim([-10, 50])
+# plt.ylim([360, 500])
 plt.xlabel('x (m)')
 plt.ylabel('y (m)')
 plt.legend()
