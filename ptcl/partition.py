559c42e469cfeb3425e5b39e91d773e8107ddbc0
ry4nzhu <ryanzhu@umich.edu>
2021-07-21 17:55:33 -0400

    compare different encoding

--- ptcl/partition.py
+++ ptcl/partition.py
@@ -1,6 +1,8 @@
 import numpy as np
-import sys
+import sys, os
 import time
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import pointcloud
 
 PCD_DIR = ['../DeepGTAV-data/object-0227-1/velodyne_2/', \
 '../DeepGTAV-data/object-0227-1/alt_perspective/0022786/velodyne_2/',\
@@ -16,8 +18,19 @@ def simple_partition(pcd, range, sample_rate=16):
     out_of_range_mask = dist_to_center > (range*range)
     partitioned_pcd = pcd[in_range_mask]
     out_of_range_points = pcd[out_of_range_mask]
-    sampled = out_of_range_points[::sample_rate,]
+    sampled = out_of_range_points[::sample_rate,:]
     partitioned_pcd = np.vstack((partitioned_pcd, sampled))
+    return partitioned_pcd
+
+def simple_partition_no_sample(pcd, range, sample_rate=16):
+    xy_square = np.square(pcd[:, :2])
+    dist_to_center = np.sum(xy_square, axis=1)
+    in_range_mask = dist_to_center <= (range*range)
+    out_of_range_mask = dist_to_center > (range*range)
+    partitioned_pcd = pcd[in_range_mask]
+    out_of_range_points = pcd[out_of_range_mask]
+    sampled = out_of_range_points[::sample_rate,:]
+    # partitioned_pcd = np.vstack((partitioned_pcd, sampled))
     return partitioned_pcd
 
 
@@ -36,12 +49,24 @@ if __name__ == "__main__":
         pcd = np.fromfile(PCD_DIR[i]+sys.argv[1], dtype=np.float32).reshape([-1, 4])
         t_s = time.time()
         partitioned_pcd = simple_partition(pcd, 20, sample_rate=16)
+        partitioned_pcd = pcd
+        print("ptcl shape " + str(partitioned_pcd.shape))
+        pc, _ = pointcloud.dracoEncode(partitioned_pcd, 10, 12)
+        print("Encoded size:", len(pc))
+        partitioned_pcd = simple_partition_no_sample(pcd, 20)
+        print("sampled shape " + str(partitioned_pcd.shape))
+        pc, _ = pointcloud.dracoEncode(partitioned_pcd, 10, 12)    
+        print("Encoded size:", len(pc))
+        partitioned_pcd, partitioned_pcd_1, partitioned_pcd2, partitioned_pcd3 = layered_partition(pcd, [5, 8, 15])
+        print("layered shape " + str(partitioned_pcd.shape))
         t_elapsed = time.time() - t_s
         print(t_elapsed)
         print(pcd.shape)
         print(partitioned_pcd.shape)
         print(partitioned_pcd.shape[0]/pcd.shape[0])
+        pcd, _ = pointcloud.dracoEncode(partitioned_pcd, 10, 12)
+        print(len(pcd))
         # with open('original.bin', 'w') as f:
         #     pcd.tofile(f)
-        with open('sample%d.bin'%i, 'w') as f:
-            partitioned_pcd.tofile(f)
+        # with open('sample%d.bin'%i, 'w') as f:
+        #     partitioned_pcd.tofile(f)
