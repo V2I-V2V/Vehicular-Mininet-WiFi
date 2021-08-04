5b99d448a2e8ce8b51a654bc24c06f03edeac284
ry4nzhu <ryanzhu@umich.edu>
2021-08-03 21:37:11 -0400

    add a functionaly on plot location at cerntain timestamp

--- analysis/trajectory.py
+++ analysis/trajectory.py
@@ -41,19 +41,57 @@ def plot_distance_for_each_node(trajs, save_dir='./'):
             if start_node != end_node:
                 start_node_traj, end_node_traj = trajs[start_node], trajs[end_node]
                 dist = np.linalg.norm(start_node_traj - end_node_traj, axis=1)
-                ax.plot(np.arange(0, len(dist)), dist, label='dist to %d'%end_node)
-                # ax.set_ylabel('distance (m)')
+                if len(dist) > 10:
+                    ax.plot(np.arange(0, int(len(dist)/10), 0.1), dist, label='dist to %d'%end_node)
+                else:
+                    ax.plot(np.arange(0, len(dist)), dist, label='dist to %d'%end_node)
+                # ax.set_xticks(np.arange(0, math))
+                # if len(dist) > 10:
+                #     ax.set_xticks(np.arange(0, len(dist)/10))
+                ax.set_ylabel('distance (m)')
+                ax.set_xlabel('time (s)')
                 ax.legend()
         cnt += 1
+        
     # plt.ylabel('y (m)')
     plt.tight_layout()
     plt.savefig(save_dir + 'node-distances.png')
 
-
+def plot_loc_at_timestamp(loc_file, timestamp, save_dir='./'):
+    traj = np.loadtxt(loc_file)
+    if traj.ndim == 1:
+        traj = traj.reshape(1, -1)
+        
+    time_idx_in_100ms = int(timestamp * 10)
+    if time_idx_in_100ms > traj.shape[0]:
+        loc = traj[-1]
+    else:
+        loc = traj[time_idx_in_100ms]
+    
+    fig = plt.figure(figsize=(7,7))
+    ax = fig.add_subplot(111)
+    for i in range(int(len(loc)/2)):
+        ax.scatter([loc[2*i]], [loc[2*i+1]], s=34, label='node%d'%i)
+    plt.xlabel('x (m)')
+    plt.ylabel('y (m)')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(save_dir + 'loc-at-%ds.png'%timestamp)
+    
 
 
 if __name__ == '__main__':
-    plot_trajectory(sys.argv[1])
+    if len(sys.argv) == 2:
+        plot_trajectory(sys.argv[1])
+    elif len(sys.argv) == 3:
+        plot_loc_at_timestamp(sys.argv[1], float(sys.argv[2]))
+    else:
+        print("To plot trajectory, use python3 trajectory.py <path_to_loc_trace>, \
+            to plot location at cerntain timestamp, use python trajectory.py \
+                <path_to_loc_trace> <time in seconds>")
+        sys.exit(1)
+    
+    
 
     # fig = plt.figure(figsize=(7,7))
     # ax = fig.add_subplot(111)
