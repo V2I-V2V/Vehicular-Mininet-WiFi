3b96921b2b2f96645116a6b6765ab515e98c6b1b
ry4nzhu <ryanzhu@umich.edu>
2021-08-11 00:21:16 -0400

    add support to print encode decisions for SSIM analysis

--- vehicle/vehicle.py
+++ vehicle/vehicle.py
@@ -183,6 +183,7 @@ def get_encoded_frame(frame_id, metric):
     if ADAPTIVE_ENCODE_TYPE == NO_ADAPTIVE_ENCODE:
         cnt = 1
         encoded_frame = pcd_data_buffer[frame_id % config.MAX_FRAMES]
+        print("frame id: " + str(frame_id) + " latency: " + str(metric) + " number of chunks: " + str(1))
     elif ADAPTIVE_ENCODE_TYPE == ADAPTIVE_ENCODE:
         encoded_frame = pcd_data_buffer[frame_id % config.MAX_FRAMES][0]
         cnt = 1
@@ -200,7 +201,7 @@ def get_encoded_frame(frame_id, metric):
         cnt = 4
         encoded_frame = pcd_data_buffer[frame_id % config.MAX_FRAMES][0] + \
                         pcd_data_buffer[frame_id % config.MAX_FRAMES][1] + \
-                        pcd_data_buffer[frame_id % config.MAX_FRAMES][2] + \
+                        pcd_data_buffer[fracdme_id % config.MAX_FRAMES][2] + \
                         pcd_data_buffer[frame_id % config.MAX_FRAMES][3]
         print("frame id: " + str(frame_id) + " latency: " + str(metric) + " number of chunks: " + str(4))
     return encoded_frame, cnt
