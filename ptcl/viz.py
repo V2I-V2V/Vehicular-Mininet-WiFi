28cec097fd7a195bedb87076ea890ec84d1bf0c5
ry4nzhu <ryanzhu@umich.edu>
2021-09-29 23:03:52 -0400

    add modified kernel v5.8 patch & update visualization script to be general

--- ptcl/viz.py
+++ ptcl/viz.py
@@ -5,20 +5,27 @@ import numpy as np
 import open3d as o3d
 import sys
 
-pcl = np.fromfile(sys.argv[1], dtype=np.float32, count=-1).reshape([-1,4])
+filename = sys.argv[1]
+if '.bin' in filename:
+	ptcl = np.fromfile(sys.argv[1], dtype=np.float32, count=-1).reshape([-1,4])
+elif '.pcd' in filename:
+	ptcl = o3d.io.read_point_cloud(filename)
+	ptcl = np.asarray(ptcl.points)
+elif '.npy' in filename:
+	ptcl = np.load(filename)
+	
 
 count = 0
-print(pcl.shape[0])
-# print(pcl[:10])
-for p in pcl:
+print(ptcl.shape[0])
+for p in ptcl:
 	if np.isnan(p[0]):
 		count = count + 1
 # print(count)
-pcl = pcl[np.logical_not(np.isnan(pcl[:,0]))]
-print(pcl.shape[0])
+ptcl = ptcl[np.logical_not(np.isnan(ptcl[:,0]))]
+print(ptcl.shape[0])
 
 pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(pcl[:,:3])
+pcd.points = o3d.utility.Vector3dVector(ptcl[:,:3])
 pcd.paint_uniform_color([0, 0, 1])
 o3d.visualization.draw_geometries([pcd])
 
