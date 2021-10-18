a3ece2104a2802bf3bb6ca825b01374c3e5acfe9
ry4nzhu <ryanzhu@umich.edu>
2021-10-18 02:35:42 -0400

    grouping code uploaded

--- ptcl/pcd_stresstest.py
+++ ptcl/pcd_stresstest.py
@@ -8,7 +8,7 @@ import getpass
 if __name__ == '__main__':
     usrname = getpass.getuser()
     ptcl = ptcl_utils.read_ptcl_data('/home/'+usrname+'/carla-bin/86/velodyne/1000.bin')
-    encoded, ratio = pointcloud.dracoEncode(ptcl, 10, 10)
+    encoded, ratio = pointcloud.dracoEncode(ptcl, 10, 8)
     decoded = pointcloud.dracoDecode(encoded)
     print(ratio)
     print(ptcl.shape[0], decoded.shape[0])
